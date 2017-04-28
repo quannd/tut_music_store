3e869d3735d1b9dc662e8bc78e09431c2348ad27
Nguyen Duc Quan <quan.nguyenduc@gmail.com>
2017-04-28 13:56:59 +0700

    Create new source

---
+++ MvcMusicStore/Models/mycss.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class mycss:  System.Web.UI.MasterPage
+    {
+        private string _bodyId;
+        public string BodyId
+        {
+            get { return _bodyId; }
+            set { _bodyId = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
